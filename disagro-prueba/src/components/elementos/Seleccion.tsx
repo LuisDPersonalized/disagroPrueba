4d0d6572fa3c9bb9deec5112075ad178f405ea0e
LuisDPersonalized <luis.edo.1999@gmail.com>
2026-09-20 23:24:03 -0600

    agregar props a sleccion

--- disagro-prueba/src/components/elementos/Seleccion.tsx
+++ disagro-prueba/src/components/elementos/Seleccion.tsx
@@ -1,8 +1,15 @@
-export function Seleccion(){
+interface SeleccionProps{
+    id:number;
+    nombre: string;
+    precio: number;
+    esProducto: number;
+}
+
+export function Seleccion({id,nombre,precio,esProducto}: SeleccionProps){
     <div>
         <label>
-        <input type="checkbox" name="selectionBox" /> Producto
+        <input type="checkbox" name="selectionBox" /> {nombre}
         </label>
-        <label>Precio</label>
+        <label>{precio}</label>
     </div>
 }
